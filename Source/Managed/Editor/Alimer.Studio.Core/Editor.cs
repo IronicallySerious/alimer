d8b50f88c6de372cbcbeba5cc1e62643813f5a3a
Amer Koleci <amerkoleci@gmail.com>
2018-07-16 11:56:27 +0200

    Revert "Remove editor from source code."
    This reverts commit 3b5311f2f6c9b27c9763c9a1c5310a6fdcd0afdd.

---
+++ Source/Managed/Editor/Alimer.Studio.Core/Editor.cs
@@ -0,0 +1,12 @@
+// Copyright (c) Amer Koleci and contributors.
+// Licensed under the Apache License, Version 2.0.
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace Alimer.Studio
+{
+	public sealed class Editor : Application
+    {
+	}
+}
