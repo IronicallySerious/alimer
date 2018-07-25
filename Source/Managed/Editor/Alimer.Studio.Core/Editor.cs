85214be4775a971f291162773cb074c783701b9f
Amer Koleci <amerkoleci@gmail.com>
2018-07-25 08:15:58 +0200

    Remove C# editor.

--- Source/Managed/Editor/Alimer.Studio.Core/Editor.cs
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Amer Koleci and contributors.
-// Licensed under the Apache License, Version 2.0.
-
-namespace Alimer.Studio
-{
-	public sealed class Editor : Application
-    {
-	}
-}
