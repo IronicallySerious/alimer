9d98c8ca2bd74b29a41a5ef510965d149bdae508
Amer Koleci <amerkoleci@gmail.com>
2018-11-06 20:02:13 +0100

    Remove C# projects and rttr integration.

--- Source/Managed/core/AlimerSharp/Runtime/PreserveAttribute.cs
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Amer Koleci and contributors.
-// Licensed under the MIT License.
-
-public sealed class PreserveAttribute : System.Attribute
-{
-    public bool AllMembers;
-    public bool Conditional;
-}
