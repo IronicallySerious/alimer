f57245a8e26a934b16bcfc1d8f99c7416d844bde
Amer Koleci <amerkoleci@gmail.com>
2018-11-24 13:09:52 +0100

    Commit missing files.

---
+++ Source/Alimer/Debug/Log.cpp
@@ -0,0 +1,335 @@
+//
+// Copyright (c) 2018 Amer Koleci and contributors.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+#include "../Debug/Log.h"
+#include "../IO/FileStream.h"
+#include <cstdio>
+#include <cstdlib>
+#include <cstdarg>
+#include <vector>
+#include <ctime>
+
+#if VORTEX_PLATFORM_IOS || ALIMER_PLATFORM_TVOS
+#include <sys/syslog.h>
+#endif
+
+#if ALIMER_PLATFORM_WINDOWS || ALIMER_PLATFORM_UWP
+#ifndef NOMINMAX
+#   define NOMINMAX
+#endif
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#include <strsafe.h>
+#endif
+
+namespace Alimer
+{
+    static const char *LogLevelPrefix[static_cast<unsigned>(LogLevel::Count)] = {
+        "TRACE",
+        "DEBUG",
+        "INFO",
+        "WARN",
+        "ERROR",
+        "CRITICAL"
+    };
+
+#if ALIMER_DEV && !ALIMER_PLATFORM_UWP 
+    WORD SetConsoleAttribs(HANDLE consoleHandle, WORD attribs)
+    {
+        CONSOLE_SCREEN_BUFFER_INFO orig_buffer_info;
+        GetConsoleScreenBufferInfo(consoleHandle, &orig_buffer_info);
+        WORD back_color = orig_buffer_info.wAttributes;
+        // retrieve the current background color
+        back_color &= static_cast<WORD>(~(FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY));
+        // keep the background color unchanged
+        SetConsoleTextAttribute(consoleHandle, attribs | back_color);
+        return orig_buffer_info.wAttributes; // return orig attribs
+    }
+#endif
+
+    String GetTimeStamp()
+    {
+        char dateTime[20];
+        time_t sysTime;
+        time(&sysTime);
+        tm* timeInfo = localtime(&sysTime);
+        strftime(dateTime, sizeof(dateTime), "%Y-%m-%d %H:%M:%S", timeInfo);
+        return String(dateTime);
+    }
+
+    static Alimer::Logger* __logInstance = nullptr;
+
+    Logger::Logger()
+        : _logFile(nullptr)
+    {
+#ifdef _DEBUG
+        SetLevel(LogLevel::Debug);
+#else
+        SetLevel(LogLevel::Info);
+#endif
+
+#if ALIMER_DEV
+        AllocConsole();
+#endif
+        AddSubsystem(this);
+        __logInstance = this;
+    }
+
+    Logger::~Logger()
+    {
+        Close();
+        RemoveSubsystem(this);
+        __logInstance = nullptr;
+    }
+
+    void Logger::Open(const String& fileName)
+    {
+        if (fileName.IsEmpty())
+            return;
+
+        if (_logFile && _logFile->IsOpen())
+        {
+            if (_logFile->GetName() == fileName)
+            {
+                return;
+            }
+
+            Close();
+        }
+
+        _logFile = new FileStream();
+        if (_logFile->Open(fileName, FileAccess::WriteOnly))
+        {
+            Log(LogLevel::Info, String::Format("Opened log file '%s'", fileName.CString()));
+        }
+        else
+        {
+            SafeDelete(_logFile);
+            Log(LogLevel::Error, String::Format("Failed to create log file '%s'", fileName.CString()));
+        }
+    }
+
+    void Logger::Close()
+    {
+        if (_logFile
+            && _logFile->IsOpen())
+        {
+            SafeDelete(_logFile);
+        }
+    }
+
+    void Logger::SetLevel(LogLevel newLevel)
+    {
+        _level = newLevel;
+    }
+
+    void Logger::Log(LogLevel level, const String& message)
+    {
+        if (_level > level)
+            return;
+
+        // Log to the default output
+        OnLog(level, message);
+    }
+
+    void Logger::Trace(const String& message)
+    {
+        if (_level > LogLevel::Trace)
+            return;
+
+        OnLog(LogLevel::Trace, message);
+    }
+
+    void Logger::Debug(const String& message)
+    {
+        if (_level > LogLevel::Debug)
+            return;
+
+        OnLog(LogLevel::Debug, message);
+    }
+
+    void Logger::Info(const String& message)
+    {
+        if (_level > LogLevel::Info)
+            return;
+
+        OnLog(LogLevel::Info, message);
+    }
+
+    void Logger::Warn(const String& message)
+    {
+        if (_level > LogLevel::Warn)
+            return;
+
+        OnLog(LogLevel::Warn, message);
+    }
+
+    void Logger::Error(const String& message)
+    {
+        if (_level > LogLevel::Error)
+            return;
+
+        OnLog(LogLevel::Error, message);
+    }
+
+    void Logger::AddListener(LogListener* listener)
+    {
+        ALIMER_VERIFY(listener != nullptr);
+
+        _listeners.push_back(listener);
+    }
+
+    void Logger::RemoveListener(LogListener* listener)
+    {
+        ALIMER_VERIFY(listener != nullptr);
+
+
+        for (auto it = _listeners.begin(); it != _listeners.end(); ++it)
+        {
+            if ((*it) == listener)
+            {
+                _listeners.erase(it);
+                break;
+            }
+        }
+    }
+
+    void Logger::OnLog(LogLevel level, const String& message)
+    {
+#if ALIMER_PLATFORM_WINDOWS || ALIMER_PLATFORM_UWP
+        size_t length = strlen(LogLevelPrefix[static_cast<unsigned>(level)]) + 2 + message.Length() + 1 + 1 + 1;
+        char* output = new char[length];
+        snprintf(output, length, "%s: %s\r\n", LogLevelPrefix[static_cast<unsigned>(level)], message.CString());
+
+        std::vector<wchar_t> szBuffer(length);
+        if (MultiByteToWideChar(CP_UTF8, 0, output, -1, szBuffer.data(), static_cast<int>(szBuffer.size())) == 0)
+            return;
+
+        OutputDebugStringW(szBuffer.data());
+
+#if ALIMER_DEV && !ALIMER_PLATFORM_UWP 
+        HANDLE handle = nullptr;
+        switch (level)
+        {
+        case LogLevel::Warn:
+        case LogLevel::Error:
+        case LogLevel::Critical:
+            handle = GetStdHandle(STD_ERROR_HANDLE);
+            break;
+        case LogLevel::Trace:
+        case LogLevel::Info:
+        case LogLevel::Debug:
+            handle = GetStdHandle(STD_OUTPUT_HANDLE);
+            break;
+        default: break;
+        }
+
+        if (handle)
+        {
+            DWORD bytesWritten;
+
+            WriteConsoleA(handle,
+                "[",
+                1,
+                &bytesWritten,
+                nullptr);
+
+            const WORD BOLD = FOREGROUND_INTENSITY;
+            const WORD RED = FOREGROUND_RED;
+            const WORD GREEN = FOREGROUND_GREEN;
+            const WORD CYAN = FOREGROUND_GREEN | FOREGROUND_BLUE;
+            const WORD WHITE = FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE;
+            const WORD YELLOW = FOREGROUND_RED | FOREGROUND_GREEN;
+
+            DWORD attribs = 0;
+            switch (level)
+            {
+            case LogLevel::Trace:
+                attribs = WHITE;
+                break;
+
+            case LogLevel::Debug:
+                // Cyan 
+                attribs = CYAN;
+                break;
+
+            case LogLevel::Info:
+                attribs = GREEN;
+                break;
+
+            case LogLevel::Warn:
+                attribs = YELLOW | BOLD;
+                break;
+
+            case LogLevel::Error:
+                attribs = RED | BOLD;
+                break;
+
+            case LogLevel::Critical:
+                attribs = BACKGROUND_RED | WHITE | BOLD; // white bold on red background
+                break;
+            }
+
+            const char* levelMessage = LogLevelPrefix[static_cast<unsigned>(level)];
+
+            DWORD origAttribs = SetConsoleAttribs(handle, attribs);
+            WriteConsoleA(handle,
+                levelMessage,
+                static_cast<DWORD>(strlen(levelMessage)),
+                &bytesWritten,
+                nullptr);
+            ::SetConsoleTextAttribute(handle, origAttribs); // Reset to original colors
+
+            String printMessage = "] " + message;
+            printMessage += "\r\n";
+
+            WriteConsoleA(handle,
+                printMessage.CString(),
+                static_cast<DWORD>(printMessage.Length()),
+                &bytesWritten,
+                nullptr);
+        }
+#endif 
+#endif
+
+        if (_logFile)
+        {
+            String formattedMessage = 
+                GetTimeStamp() + "[" +  LogLevelPrefix[static_cast<uint32_t>(level)] + "] ";
+            formattedMessage += ": " + message;
+
+            _logFile->WriteLine(formattedMessage);
+            _logFile->Flush();
+        }
+
+        // Log listeners.
+        for (auto listener : _listeners)
+        {
+            listener->MessageLogged(level, message);
+        }
+    }
+
+    Logger& gLog()
+    {
+        return *__logInstance;
+    }
+}
---
+++ Source/Alimer/Debug/Log.h
@@ -0,0 +1,134 @@
+//
+// Copyright (c) 2018 Amer Koleci and contributors.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+#pragma once
+
+#include "../Base/Ptr.h"
+#include "../Core/Object.h"
+#include <memory>
+#include <vector>
+
+namespace Alimer
+{
+    enum class LogLevel : uint8_t
+    {
+        Trace = 0,
+        Debug = 1,
+        Info = 2,
+        Warn = 3,
+        Error = 4,
+        Critical = 5,
+        Count
+    };
+
+    class FileStream;
+
+    /// Listener interface for Log events.
+    class ALIMER_API LogListener
+    {
+    public:
+        virtual ~LogListener() { }
+
+        /// Called when message is being logged.
+        virtual void MessageLogged(LogLevel level, const String& message) = 0;
+    };
+
+    /// Class for logging functionalities.
+    class ALIMER_API Logger final : public Object
+    {
+        ALIMER_OBJECT(Logger, Object);
+
+    public:
+        /// Construcor.
+        Logger();
+
+        /// Destructor.
+        ~Logger();
+
+        /// Open the log file.
+        void Open(const String& fileName);
+        /// Close the log file.
+        void Close();
+
+        /// Set logging level.
+        void SetLevel(LogLevel newLevel);
+
+        /// Return logging level.
+        LogLevel GetLevel() const { return _level; }
+
+        void Log(LogLevel level, const String& message);
+        void Trace(const String& message);
+        void Debug(const String& message);
+        void Info(const String& message);
+        void Warn(const String& message);
+        void Error(const String& message);
+
+        /// Adds a log listener.
+        void AddListener(LogListener* listener);
+
+        /// Removes a log listener.
+        void RemoveListener(LogListener* listener);
+
+        /// Return instance of opened log file.
+        const FileStream* GetLogFile() const { return _logFile; }
+
+    private:
+        void OnLog(LogLevel level, const String& message);
+
+    private:
+        LogLevel _level;
+
+        /// Log file.
+        FileStream* _logFile;
+
+        /// List of Listener's on the Log.
+        std::vector<LogListener*> _listeners;
+
+    private:
+        DISALLOW_COPY_MOVE_AND_ASSIGN(Logger);
+    };
+
+    /// Access to Logger module.
+    ALIMER_API Logger& gLog();
+}
+
+#define ALIMER_LOGTRACE(message) Alimer::gLog().Log(Alimer::LogLevel::Trace, message)
+#define ALIMER_LOGDEBUG(message) Alimer::gLog().Log(Alimer::LogLevel::Debug, message)
+#define ALIMER_LOGINFO(message) Alimer::gLog().Log(Alimer::LogLevel::Info, message)
+#define ALIMER_LOGWARN(message) Alimer::gLog().Log(Alimer::LogLevel::Warn, message)
+#define ALIMER_LOGERROR(message) Alimer::gLog().Log(Alimer::LogLevel::Error, message)
+#define ALIMER_LOGCRITICAL(message) do { \
+	Alimer::gLog().Log(Alimer::LogLevel::Critical, message); \
+	ALIMER_BREAKPOINT(); \
+	ALIMER_UNREACHABLE(); \
+} while (0)
+
+#define ALIMER_LOGTRACEF(format, ...) Alimer::gLog().Log(Alimer::LogLevel::Trace, Alimer::String::Format(format, __VA_ARGS__))
+#define ALIMER_LOGDEBUGF(format, ...) Alimer::gLog().Log(Alimer::LogLevel::Debug, Alimer::String::Format(format, __VA_ARGS__))
+#define ALIMER_LOGINFOF(format, ...) Alimer::gLog().Log(Alimer::LogLevel::Info, Alimer::String::Format(format, __VA_ARGS__))
+#define ALIMER_LOGWARNF(format, ...) Alimer::gLog().Log(Alimer::LogLevel::Warn, Alimer::String::Format(format, __VA_ARGS__))
+#define ALIMER_LOGERRORF(format, ...) Alimer::gLog().Log(Alimer::LogLevel::Error, Alimer::String::Format(format, __VA_ARGS__))
+#define ALIMER_LOGCRITICALF(format, ...) do { \
+	Alimer::gLog().Log(Alimer::LogLevel::Critical, Alimer::String::Format(format, __VA_ARGS__)); \
+	ALIMER_BREAKPOINT(); \
+	ALIMER_UNREACHABLE(); \
+} while (0)
