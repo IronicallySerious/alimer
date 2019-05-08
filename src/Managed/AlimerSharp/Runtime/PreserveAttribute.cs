6bf538eeece6d6de5c447bd8536d4b3f693567aa
Amer Koleci <amerkoleci@gmail.com>
2019-05-08 11:47:34 +0200

    Remove all source, will add back later.

--- src/Managed/AlimerSharp/Runtime/PreserveAttribute.cs
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Amer Koleci and contributors.
-// Distributed under the MIT license. See the LICENSE file in the project root for more information.
-
-[System.AttributeUsage(System.AttributeTargets.All, AllowMultiple = false)]
-public sealed class PreserveAttribute : System.Attribute
-{
-    public bool AllMembers;
-    public bool Conditional;
-}
